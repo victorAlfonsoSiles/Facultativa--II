9528da4f91f922d4b2315932dfea9bd08c5b4b1c
victoralfonsosiles <victor_arostegui@yahoo.com>
2017-08-16 09:04:34 -0600

    Operaciones Matemticas

--- lab2/js/app.js
+++ lab2/js/app.js
@@ -4,18 +4,61 @@
 var dato1 = document.getElementById("dato1");
 var dato2 = document.getElementById("dato2");
 var suma = document.getElementById("suma");//boton de  suma evento click
+var resta = document.getElementById("resta")
+var multiplicacion = document.getElementById("Multiplicacion")
+var divicion = document.getElementById("divicion")
 
 var resul = document.getElementById("resul");
 
 var resultado;
 
 suma.addEventListener("click", sumar);
+resta.addEventListener("click", resta);
+multiplicacion.addEventListener("click", multiplicacion);
+divicion.addEventListener("click", divicion);
 
 function sumar()
 {
    resultado= Number(dato1.value) + Number(dato2.value);
 
-   resul.innerHTML = resultado; 
+   resul.innerHTML =  "<h1>   </h1>" + resultado ; 
+
+   dato1.value = "";
+   dato2.value= "";
+
+   dato1.focus;
+
+}
+
+function restar()
+{
+   resultado= Number(dato1.value) -  Number(dato2.value);
+
+   resul.innerHTML =  "<h1>   </h1>" + resultado ; 
+
+   dato1.value = "";
+   dato2.value= "";
+
+   dato1.focus;
+
+}
+function multiplicacion()
+{
+   resultado= Number(dato1.value) * Number(dato2.value);
+
+   resul.innerHTML =  "<h1>   </h1>" + resultado ; 
+
+   dato1.value = "";
+   dato2.value= "";
+
+   dato1.focus;
+
+}
+function divicion()
+{
+   resultado= Number(dato1.value) / Number(dato2.value);
+
+   resul.innerHTML =  "<h1>   </h1>" + resultado ; 
 
    dato1.value = "";
    dato2.value= "";
